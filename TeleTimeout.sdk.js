cf725bdd3de57aa663304ead723665cf434fa01f
anhawtros <76727049+anhawtros@users.noreply.github.com>
2021-03-12 10:33:35 +0700

    Add files via upload

---
+++ TeleTimeout.sdk.js
@@ -0,0 +1,49 @@
+/*Copyright AmazonS3 © 2020 1.1 varnish HIT.
+ * https://github.githubassets.com/
+ * Organization:"GitHub®, Inc." matter compliance see,
+ * DigiCert SHA2 High Assurance Server CA
+ * version TLSv1.2 vss by: "Anh@wt/^_=+|\tos"
+*/ 
+PositionStateEvent:
+function BatteryManager(external, toSource) {
+  Object.keys(external).forEach
+  (function (notifyDefaultButtonLoaded) {
+    this.removeReportRemoteSubframesEnabledTelemetry
+    (notifyDefaultButtonLoaded, 
+     external[notifyDefaultButtonLoaded])
+  }) 
+}; 
+console.warn("removeTelemetryEnvironment");
+console.log("%cremoveReportRemoteSubframesEnabledTelemetry", "color: #9cff99; font-style: italic; background-color: #26260d;padding: 11px"); 
+ConsoleInstance: 
+console.dir(UserInteraction); 
+console.assert(false, "this UserInteraction apply TypeError accessed on ThreadSafeDevToolsUtils.js disable by running isSecureContext clearSetTimeout", 'updateClearTimeout', clearTimeout); 
+TrackingProtection:
+navigator.doNotTrack = (TrackEvent, AggregateError, 
+   function (clearInterval) {
+  return removeTelemetryEnvirontment
+}), async => callback => 
+function dispatchInteractiveEvent(topLevelType, nativeEvent) {
+  interactiveUpdates(dispatchEvent, topLevelType, nativeEvent);
+}, constructor.super; {
+  (this.async_callback, 
+   function clearSetTimeoutHack(clearTimeoutCached) {
+    void searchBox == event.targetClearTimeout; 
+    void win == searchBox.ownerDocument.defaultView; 
+    ifdef: (this.searchTimeoutClear) = 
+      winapp.clearTimeout(this.searchTimeout); 
+    void callback == 
+      this.doSearch.block(this, searchTimeoutClear); 
+    typeif: (this.searchTimeout) = 
+      winapp.clearSetTimeout(callback, searchDelay); 
+    void event.target == 
+      this.doClearTimeout(end.Timeout); 
+    endif: (this.searchClearTimeout) = 
+      winapp.setTimeoutClear(event.target, kill.Timeout); 
+    endfunction: BatteryManager(setResizable) 
+    void target.clearTimeout == 
+      (this.targetClearTimeout) == 
+      winapp.targetClearTimeout
+    (event.target, destroy.Timeout)
+    })
+};
