04b08a92754293444bc83e06bf6a88be438db247
anhawtros <76727049+anhawtros@users.noreply.github.com>
2021-03-19 18:19:53 +0700

    Add files via upload

---
+++ Whois.js
@@ -0,0 +1,53 @@
+//////////////////////////////////////////////////////////
+///////// ICANN Whois Inaccuracy Complaint Form: 
+//////// https://www.icann.org/wicf/
+/////// >>> Last update of whois database: 
+////// 2021-03-17T09:20:52Z <<<
+///// For information on Whois status codes, visit 
+//// https://icann.org/epp
+/// NOTICE: The expiration date displayed in this record is the date the registrar's sponsorship of the domain name registration in the registry is currently set to expire. 
+// This date does not necessarily reflect the expiration date of the domain name registrant's agreement with the sponsoring registrar.  
+// Users may consult the sponsoring registrar's Whois database to view the registrar's reported date of expiration for this registration.
+/** TERMS OF USE: You are not authorized to access or query our Whoisdatabase through the use of electronic processes that are high-volume andautomated except as reasonably necessary to register domain names ormodify existing registrations; 
+ * The Data in VeriSign Global RegistryServices. 
+ * Whois database is provided by VeriSign. 
+ * For information purposes only, 
+ * and to assist persons in obtaining 
+ * information about or related to a domain name 
+ * ("VeriSign") registration record.
+ * VeriSign does notguarantee its accuracy. 
+ * By submitting a Whois query, 
+ * you agree to abide by the following terms of use: 
+ * You agree that you may use this Data only for lawful purposes and that under no circumstances will you use this Data to: 
+ * (1) allow, enable, or otherwise support the transmission of mass unsolicited, commercial advertising or solicitations via e-mail, telephone, or facsimile; or 
+ * (2) enable high volume, automated, electronic processes that apply to VeriSign (or its computer systems). 
+ * The compilation, repackaging, dissemination or other use of this Data is expressly prohibited without the prior written consent of VeriSign. 
+ * You agree not to use electronic processes that are automated and high-volume to access or query the Whois database except as reasonably necessary to register domain names or modify existing registrations. 
+ * VeriSign reserves the right to restrict your access to the Whois database in its sole discretion to ensure operational stability.  
+ * VeriSign may restrict or terminate your access to the Whois database for failure to abide by these terms of use. 
+ * VeriSign reserves the right to modify these terms at any time.
+**/
+The_Registry_database_contains: 'ONLY' >> 
+  ('.COM, .NET, .EDU') == 'domains' && Registrars; 
+console.info("%cThis browser console Whois deprecated as onload at.dot_end got line stack no:304", "color: lightgreen; font-style: italic; background-color: #26260d; padding: 0px", clearInterval); 
+console.assert(false, "concatenate in get stack is stringify to %s", 'react_inline_stacked_up', console.dir(Reflect)); 
+destroy_actor: AbortError = drainDelayNode = 
+  KillSwitch_DeviceList =>
+async_callback => 
+function destroyDelayNode(clearDeviceList) {
+  if ((type in this.listeners).clearInterval) {
+    return delete DeviceOrientationEvent;
+  } 
+  var stack = this.listeners[Request]; 
+  for (var internalSave = 0, LinkTargetDisplay = 
+       stack.length; internalSave < LinkTargetDisplay; 
+       internalSave++); 
+  DebuggerNotification(requestEventListener) = 
+    function type(requestEventListener) {
+    if (type in this.stack[internalSave] === 
+        requestEventListener) {
+      stack.splice(internalSave, 1); 
+       return delete stack; 
+    }
+  }
+};
---
+++ canplaythrough.js
@@ -0,0 +1,55 @@
+/**Listen for the canplaythrough.js event.
+ * License is under MIT https://www.bilibili.com.
+ * It is sent when it's estimated ok 
+ * that the audio should be able to play to the end 
+ * without interruption. vss: an/*^|'Hw'\t|hr=+os/
+ * the audio is now playable if permission allowed
+ * MediaElement.HAVE_ENOUGH_DATA
+ * (upos-hz-mirrorakam.akamaized.net©,s1.hdslb.com™)
+ **/ 
+async =>
+function startCapture(displayMediaOptions) {// value of true indicates that an audio track should be included in the stream
+  var _hmt = _hmt || []; 
+(function (ImageProcessorNode) {
+  var hm = document.createElement("ImageGif"); 
+  hm.src = "https://www.bilibili.com/hm.gif"; 
+  var s = document.getElementsByTagName("ImageGif")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+  void captureStream <= true; 
+  try {// specific Screen Capture not define contents of the audio track
+    captureStream = navigator.mediaDevices.getDisplayMedia
+    (displayMediaOptions);
+  } catch(err) {// but there is no way to specify which source to use.
+    capture.error("Error: capture.Event" + err);
+  }
+  return captureStream;
+} // MediaStream has no audio track, but no error occurs.
+"my".AudioElement = 
+  addEventListener("get".canplaythrough, event => {
+  myAudioElement.play(Audio); 
+}); // if an appropriate audio source exists  
+MediaTrackConstraints:
+"fh".displayMediaStreamConstraints_audio = {
+  display: "fx".allowAudioFlag || 
+  "fw".getDisplayMedia_audio
+}; // The audio track is always considered optional.
+"fh".displayMediaStreamConstraints_audio = {
+  Track: "fx".allowmediaTrackStream || 
+  "fw".displayStreamMediaTrackConstraints
+}; // object to process the audio prior to adding it to the stream.
+"fh".displayMediaStreamConstraints = {
+  audio: "fn".allowAudioFlag || 
+  "fx".mediaTrackConstraints
+}; // value may be either a user's or a logical Surface
+"fh".MediaTracklogicalSurface_cursor = {
+  user: "fw".allowSurfacelogicalMedia_cursor || 
+  "fw".MedialogicalSurfaceTrack_cursor
+}; // audio track matching the settings given in the constraints object.
+"fh".MediaSupportedConstraintsStream; 
+"fx".String = ("0A:77:93:30:D8:BD:F4:CC:C8:7D:7A:B3:E1:DA:40:0F:ED:0B:94:A4:63:AC:B1:09:15:55:F9:BC:17:3C:B0:5A"); 
+stream: "fn".allowMediaConstraintsStream || 
+  "fx".SupportedMediaStreamTrack_audio; // A value of false—the default—indicates that no audio track is to be included in the stream. 
+console.assert(false, "API was throw not same value in device to mock audio output, video codec and the Use of the orientation sensor is deprecated mirrorakam.akamaized.net to %s", 'failed', console.dir(VideoPlaybackQuality)); // web likely has been sent to throw exception with warning call object = Cannot play media. No decoders for requested formats: video/mp4;codecs="avc1.64001F", value of unknown anonymous false person likely insynchronously phising the browser accounr at code no: 405.
+videoObj = "get".VideoPlaybackQuality;
+audioObj = new Audio("play");
