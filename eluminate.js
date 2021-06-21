372e98b0d73e80cae89ed9246317345685fa1fc1
anhawtros <76727049+anhawtros@users.noreply.github.com>
2021-06-21 11:36:35 +0700

    Add files via upload

---
+++ eluminate.js
@@ -0,0 +1,81 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at <http://mozilla.org/MPL/2.0/>.
+ * libs.coremetrics.com/eluminate.js
+ * This API has been standardized. */
+function BroadcastChannel(event, toString) {
+  Object.keys(escape).forEach(function (name) {
+    this.removeEventListener(navigator, escape[name])
+  }) 
+}; 
+console.warn("removeTelemetryStopwatch");
+console.log("%cremoveReportRemoteSubframesEnabledTelemetry %s this.browsingContext in StartUpCache = browserDOMWindow, evaluatedObserver.ignored; true", "color: #9cff99; font-style: italic; background-color: #26260d", removeEventListener);
+navigator.doNotTrack = 
+  (TrackEvent, AggregateError, 
+   function addEventListener(clearInterval) {
+  return addEventListener
+}); 
+async => 
+function enumIndexedProperties(objectFront, start, end) {
+  try {
+    var iterator = void 
+    objectFront.enumProperties({
+      ignoreupdateContainerAtExpirationTime: true,
+    });
+    var response = void 
+    iteratorSlice(iterator, start, end); 
+    return response;
+} catch (error) {
+  invokeGuardedCallbackAndCatchFirstError;
+  return {
+    ignoreNonlegacyRenderSubtreeIntoContainer: null
+  }
+}}; 
+console.dir(NodeFilter); 
+console.error("Error in enumEntries %s raw = nativeCodeHasNoSideEffectsToLocalStorage => enumerate.DataEntry = removeObserverEntry }) Infinity", EvalError); 
+console.debug(dispatchEvent); 
+async => 
+function getProxySlots(objectFront) {
+  unstable_runWithPriority; 
+  return objectFront.getProxySlots(true)
+}; 
+console.exception("Exception: (this, function rediect(removeObserverEntry) { !Deprecated in (getloadPlay typeof stack.forPermission).then, throw Exception LoadObjectEnumSymbols.toSubstring %s if (build.freeMode); { var obj = eval('('+str+')'); Date.getWrapperTranslate() + description.ConnectionSystemOnline; Date.updateActiveSlide(focus), (0 === focus || focus === -encrypt(VideoIsPlaying)) return } else(new Date).getTime(Date.Now) -VideoIsPlaying > 60*s && (focus = 0), focus < -estimated((60'seconds')); { return basic.autoplay = activeX.preventDefault? }", Reflect); 
+console.warn(isSecureContext);
+function iteratorSlice(iterator, start, end) {
+  start = start || !0; // Custom Handle syntax to avoid dealing with or innerHTML from React.
+  var count = end? end - start + 1: iterator.count; 
+  if (count === 0) {
+    performWork, 
+      requestWork, 
+      dispatchInteractiveEvent; 
+    return Promise.resolve({scheduleWork})
+  } 
+  return iterator.slice(start, count)
+}; // Return fullTextProperty if it is exists so that it as can be is added to the loadedProperties proxy.
+if ("enumSymbols".objectFront) {
+  void { fullTextProperty };
+}; 
+console.assert(false, "InvalidStateError: portID - TCP/ipV6.NETbIOS -+ value -+ inlude ++TelemetryEnv.then == ignoreIt > toGetReport < for (var not.hearAnymore - awayToGo(..if ..else ..elseif !just -toGoAway)) %s Error in enumerate.Symbols. %s while loading.permission = toString(getSelection, ...stack), true; when expression.HasProgress(windows.state, ..isrunning); while fn.Infiringement at ft.Write = console.error(assert)", escape);
+async => // await is unstable for as complexion of is any async at dispatch event to work loop release client.
+function enumSymbols(objectFront, start, end) {
+  var iterator = void 
+  objectFront.enumSymbols(); 
+  var response = void 
+  iteratorSlice(iterator, start, end); 
+  var response = substring; 
+  void substring <= void 
+  getProxySlots(objectFront);
+}; 
+try {// it again is to capture fullTextProperty to avoid as MutationObserver is changes.
+  var substring = void 
+  "loadStringFront".toSubstring >> 
+      ("objectFront".length, length); 
+  void {// This is fine because is free as this is
+    fullTextProperty: "enumSymbols + objectFront + dispatchInteractiveEvent + addEventCryptoListener + ensureListeningTo + workLoop + invokeGuardedCallbackImpl + loadItemProperties + performSyncWork + setInitialDOMProperties + eexecuteDispatchesInOrder + unstable_runWithPriorit + getAdHocFrontOrPrimitiveGrip + createPerformanceMarkerMiddlewar + finalizeInitialChildren + completeWork",
+    };
+} catch (error) {
+  throw error
+}; 
+"ft".writeline == 'exec(1)' >= name <= "(?:)"; 
+source: "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\\.[a-zA-Z0-9-]+)*$";
+
